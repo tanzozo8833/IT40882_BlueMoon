5dacd5e57f3f282ef5b28de277edebdcf036e1c3
Tran Phuc Son <tranphucson96@gmail.com>
2025-05-24 16:04:10 +0700

    Update SoHoKhau.js

--- src/app/models/SoHoKhau.js
+++ src/app/models/SoHoKhau.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 
 const SoHoKhauSchema = new Schema({
     idSoHoKhau: { type: Schema.Types.ObjectId, ref: 'SoHoKhau' },
-    soSoHoKhau: { type: String },
-    hoTenChu: { type: String },
-    thongTinThem: { type: String }
+    soSoHoKhau: { type: String , required: true, unique: true, trim: true },
+    hoTenChu: { type: String, trim: true , index : true },
+    thongTinThem: { type: String, trim: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('SoHoKhau', SoHoKhauSchema);
