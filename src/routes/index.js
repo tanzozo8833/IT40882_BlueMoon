dee82d6d2b540e31253cfb837f40197697d12cbe
Tran Phuc Son <tranphucson96@gmail.com>
2025-05-26 02:39:53 +0700

    update thông tin tài khoản

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const loginRouter = require('./login');
 const adminRouter = require('./admin');
 const userRouter = require('./user');
 const logoutRouter = require('./logout');
+const myInfoRouter = require('./myInfo');
 
 const authMiddleware = require('../app/middlewares/authMiddleware');
 
@@ -12,7 +13,7 @@ function route(app) {
     app.use(authMiddleware.ensureAuthenticated);
     app.use('/admin', authMiddleware.requireRole(['admin']), adminRouter);
     app.use('/user', authMiddleware.requireRole(['user']), userRouter);
-
+    app.use('/myInfo', authMiddleware.requireRole(['user', 'admin']), myInfoRouter);
     app.use('/logout', logoutRouter);
 }
 
