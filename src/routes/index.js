ff7cd3b650d037d5903f1f0d1e3ee54be941230b
Tran Phuc Son <tranphucson96@gmail.com>
2025-05-26 02:22:35 +0700

    Reapply "update Đăng xuất"
    This reverts commit d25c51f61bf827269e7252c4c63a571556a8749a.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const loginRouter = require('./login');
 const adminRouter = require('./admin');
 const userRouter = require('./user');
+const logoutRouter = require('./logout');
 
 const authMiddleware = require('../app/middlewares/authMiddleware');
 
@@ -11,6 +12,8 @@ function route(app) {
     app.use(authMiddleware.ensureAuthenticated);
     app.use('/admin', authMiddleware.requireRole(['admin']), adminRouter);
     app.use('/user', authMiddleware.requireRole(['user']), userRouter);
+
+    app.use('/logout', logoutRouter);
 }
 
 module.exports = route;
